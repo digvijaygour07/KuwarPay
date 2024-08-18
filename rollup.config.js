c22584966dabc3999c043d0ce7a536ee191cff67
DIGVIJAY_GOUR <digvijaygour8@gmail.com>
2024-08-18 18:11:22 +0530

    Initial commit with Rollup configuration

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'netlify/functions/api/api.js',
+  output: {
+    dir: '.netlify/functions',
+    format: 'cjs',
+  },
+  plugins: [
+    nodeResolve(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env'],
+    }),
+  ],
+};
