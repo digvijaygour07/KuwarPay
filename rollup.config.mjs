97dad532ceade4af92a4bbed9b7bf73e3db1b7cf
DIGVIJAY_GOUR <digvijaygour8@gmail.com>
2024-08-19 12:39:08 +0530

    Added Rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
+
 export default defineConfig({
   input: ['netlify/functions/api/api.js', 'api.js'],
   output: [
