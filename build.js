a96b50770a9ddf210951bba6dcf54046a8792920
KuwarPay <digvijaygour8@gmail.com>
2024-08-19 11:27:12 +0530

    Update build.js

--- build.js
+++ build.js
@@ -1,26 +1,31 @@
+// Import Rollup and plugins
 import { rollup } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
+// Input options
 const inputOptions = {
-  input: 'api.js', // Update this line to point to your api.js file
+  input: 'api.js', // Entry point of your application
   plugins: [
-    nodeResolve(),
-    babel({
+    nodeResolve(), // Resolve node modules
+    babel({ // Transpile JavaScript files
       babelHelpers: 'bundled',
       presets: ['@babel/preset-env'],
     }),
   ],
 };
 
+// Output options
 const outputOptions = {
-  file: 'bundle.js',
-  format: 'cjs',
+  file: 'bundle.js', // Output file
+  format: 'cjs', // Output format (CommonJS)
 };
 
+// Build function
 async function build() {
   const bundle = await rollup(inputOptions);
   await bundle.write(outputOptions);
 }
 
+// Run the build function
 build();
