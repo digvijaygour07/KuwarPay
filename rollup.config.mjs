a0379e65efe27a2b5ed31169bdfdaf757d40a4e0
DIGVIJAY_GOUR <digvijaygour8@gmail.com>
2024-08-19 12:08:47 +0530

    Updated package.json and resolved merge conflicts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
@@ -8,13 +7,12 @@ export default defineConfig({
   output: [
     {
       dir: '.netlify/functions',
-      format: 'esm', // Changed from 'es' to 'esm' for clarity
+      format: 'esm',
       sourcemap: true,
     },
   ],
   plugins: [
     nodeResolve({
-      // Add this to resolve node modules correctly
       resolveOnly: ['node_modules'],
     }),
     babel({
@@ -22,29 +20,4 @@ export default defineConfig({
       presets: ['@babel/preset-env'],
     }),
   ],
-=======
-import { defineConfig } from 'rollup';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
-
-export default defineConfig({
-  input: ['netlify/functions/api/api.js', 'api.js'],
-  output: [
-    {
-      dir: '.netlify/functions',
-      format: 'esm', // Changed from 'es' to 'esm' for clarity
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    nodeResolve({
-      // Add this to resolve node modules correctly
-      resolveOnly: ['node_modules'],
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env'],
-    }),
-  ],
->>>>>>> d0503de8593dd26c2f46b5e66cefb97fde4ef9be
 });
