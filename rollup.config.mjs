d0b794d5ed0aa9500314991a325259d7a5c05897
DIGVIJAY_GOUR <digvijaygour8@gmail.com>
2024-08-20 16:20:01 +0530

    Update Rollup configuration and dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,20 +3,20 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
 export default defineConfig({
-  input: 'netlify/functions/api/api.js', // Ensure this is the correct path to your input file
+  input: 'netlify/functions/api/api.js',
   output: {
-    file: 'netlify/functions/api.js', // Ensure this is the correct path for your output file
+    file: 'netlify/functions/api.js',
     format: 'esm',
-    sourcemap: true,
+    sourcemap: 'inline', // Changed from true to 'inline'
   },
   plugins: [
     nodeResolve({
-      mainFields: ['module', 'main'],
+      mainFields: ['module', 'main'], // No need to specify resolveOnly
     }),
     babel({
       babelHelpers: 'bundled',
       presets: ['@babel/preset-env'],
-      sourceMaps: true,
+      sourceMaps: 'inline', // Changed from true to 'inline'
     }),
   ],
 });
