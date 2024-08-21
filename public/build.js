2dea44d3536b652c32a1282803e99ff9b7208ee2
KuwarPay <digvijaygour8@gmail.com>
2024-08-21 12:20:40 +0530

    Add files via upload

---
+++ public/build.js
@@ -0,0 +1,26 @@
+import { rollup } from 'rollup';
+import babel from '@rollup/plugin-babel';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+const inputOptions = {
+  input: 'api.js',
+  plugins: [
+    nodeResolve(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env'],
+    }),
+  ],
+};
+
+const outputOptions = {
+  file: 'bundle.js',
+  format: 'cjs',
+};
+
+async function build() {
+  const bundle = await rollup(inputOptions);
+  await bundle.write(outputOptions);
+}
+
+build();
---
+++ public/bundle.js
@@ -0,0 +1,8 @@
+// Import your API code
+import api from './api.js';
+
+// Use the API code
+console.log(api);
+
+// Export the API code
+export default api;
